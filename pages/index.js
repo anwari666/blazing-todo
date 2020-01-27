0e384a2e3686a23271b1a6f0e8446865247dcdc9
Anwari Ilman - virmach <anwari.ilman@gmail.com>
2020-01-27 18:59:42 -0500

    chore: next/link raise warning when using <Link> without <a>

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ export default () => {
     return (
         <>
             <h1> Welcome to the static page for todolist! </h1>
-            <Link href="/firstlist">Go check the first list</Link>
+            <Link href="/firstlist">
+                <a>Go check the first list</a>
+            </Link>
         </>
     )
 }
